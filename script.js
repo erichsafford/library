ef4b376a81b3d940988bb667f62a33b905c6fa3b
Erich Gaul <erichgaul@gmail.com>
2023-08-12 07:40:20 -0500

    Add functionality for button that removes book from library

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-let myLibrary = [
-    {title: "1984", author: "George Orwell", genre: "Science Fiction"},
-    {title: "Dune", author: "Frank Herbert", genre: "Science Fiction"},
-    {title: "The Fellowship of the Ring", author:"J.R.R. Tolkien", genre: "Fantasy"},
-    {title: "Practical Magic", author: "Alice Hoffman", genre: "Magical Realism"},
-    {title: "Jurassic Park", author: "Michael Crichton", genre: "Science Fiction"},
-];
+let myLibrary = [];
 
-let cardCounter = 0
+let cardCounter = 1
 const titleInput = document.querySelector('#title')
 const authorInput = document.querySelector('#author')
 const genreInput = document.querySelector('#genre')
@@ -19,8 +13,16 @@ submitBtn.addEventListener('click', () => {
     addBookToLibrary()
 })
 
-function Book() {
-    // the constructor
+function toggleReadStatus(event) {
+    const card = event.target.closest('.bookCard')
+    let cardId
+    if (card) {
+        cardId = card.classList[1];
+    }
+    if (cardId) {
+        const clickedCard = document.querySelector(`.${cardId}`)
+        clickedCard.classList.toggle('yesReadStatus')
+    }
 }
 
 function isBookInLibrary(newBook) {
@@ -36,11 +38,25 @@ function isBookInLibrary(newBook) {
     return false
 }
 
+function removeBook(event) {
+    const card = event.target.closest('.bookCard')
+    console.log(card)
+    if (card) {
+        const cardId = card.classList[1];
+        const cardIndex = myLibrary.findIndex(book => book.bookId === cardId)
+        if (cardIndex !== -1) {
+            myLibrary.splice(cardIndex, 1)
+            card.remove()
+        }
+    }
+}
+
 function addBookToLibrary() {
     const newBook = {
         title: titleInput.value,
         author: authorInput.value,
-        genre: genreInput.value
+        genre: genreInput.value,
+        bookId: `bookCard-${cardCounter}`
     }
     if (!isBookInLibrary(newBook)) {
         myLibrary.push(newBook)
@@ -51,7 +67,6 @@ function addBookToLibrary() {
 }
 
 function displayBook() {
-    cardCounter++
     const bookCard = document.createElement('div')
     const titleText = document.createElement('h1')
     const authorText = document.createElement('p')
@@ -61,6 +76,7 @@ function displayBook() {
     const readBtn = document.createElement('button')
     bookCard.classList.add('bookCard')
     bookCard.classList.add(`bookCard-${cardCounter}`)
+    cardCounter++
     titleText.textContent = `${myLibrary[myLibrary.length - 1].title}`
     titleText.classList.add('titleText')
     authorText.textContent = `${myLibrary[myLibrary.length -1].author}`
@@ -72,6 +88,7 @@ function displayBook() {
     removeBtn.textContent = 'Remove from library'
     removeBtn.classList.add('removeBtn')
     readBtn.addEventListener('click', toggleReadStatus)
+    removeBtn.addEventListener('click', removeBook)
     bookCard.appendChild(titleText)
     bookCard.appendChild(divider)
     bookCard.appendChild(authorText)
@@ -81,52 +98,41 @@ function displayBook() {
     myShelf.appendChild(bookCard)
 }
 
-function displayAllBooks() {
-    myLibrary.forEach(book => {
-        cardCounter++
-        const bookCard = document.createElement('div')
-        const titleText = document.createElement('h1')
-        const authorText = document.createElement('p')
-        const genreText = document.createElement('p')
-        const divider = document.createElement('hr')
-        const removeBtn = document.createElement('button')
-        const readBtn = document.createElement('button')
-        bookCard.classList.add('bookCard')
-        bookCard.classList.add(`bookCard-${cardCounter}`)
-        titleText.textContent = `${book.title}`
-        titleText.classList.add('titleText')
-        authorText.textContent = `${book.author}`
-        authorText.classList.add('authorText')
-        genreText.textContent = `${book.genre}`
-        genreText.classList.add('genreText')
-        readBtn.textContent = 'Read'
-        readBtn.classList.add('readBtn')
-        readBtn.addEventListener('click', toggleReadStatus)
-        removeBtn.textContent = 'Remove from library'
-        removeBtn.classList.add('removeBtn')
-        bookCard.appendChild(titleText)
-        bookCard.appendChild(divider)
-        bookCard.appendChild(authorText)
-        bookCard.appendChild(genreText)
-        bookCard.appendChild(readBtn)
-        bookCard.appendChild(removeBtn)
-        myShelf.appendChild(bookCard)
-    })
-}
+// function displayAllBooks() {
+//     myLibrary.forEach(book => {
+//         cardCounter++
+//         const bookCard = document.createElement('div')
+//         const titleText = document.createElement('h1')
+//         const authorText = document.createElement('p')
+//         const genreText = document.createElement('p')
+//         const divider = document.createElement('hr')
+//         const removeBtn = document.createElement('button')
+//         const readBtn = document.createElement('button')
+//         bookCard.classList.add('bookCard')
+//         bookCard.classList.add(`bookCard-${cardCounter}`)
+//         titleText.textContent = `${book.title}`
+//         titleText.classList.add('titleText')
+//         authorText.textContent = `${book.author}`
+//         authorText.classList.add('authorText')
+//         genreText.textContent = `${book.genre}`
+//         genreText.classList.add('genreText')
+//         readBtn.textContent = 'Read'
+//         readBtn.classList.add('readBtn')
+//         readBtn.addEventListener('click', toggleReadStatus)
+//         removeBtn.textContent = 'Remove from library'
+//         removeBtn.classList.add('removeBtn')
+//         removeBtn.addEventListener('click', removeBook)
+//         bookCard.appendChild(titleText)
+//         bookCard.appendChild(divider)
+//         bookCard.appendChild(authorText)
+//         bookCard.appendChild(genreText)
+//         bookCard.appendChild(readBtn)
+//         bookCard.appendChild(removeBtn)
+//         myShelf.appendChild(bookCard)
+//     })
+// }
 
-function toggleReadStatus(event) {
-    const card = event.target.closest('.bookCard')
-    let cardId
-    if (card) {
-        cardId = card.classList[1];
-    }
-    if (cardId) {
-        const clickedCard = document.querySelector(`.${cardId}`)
-        clickedCard.classList.toggle('yesReadStatus')
-    }
-}
 
-displayAllBooks()
 
 
 
